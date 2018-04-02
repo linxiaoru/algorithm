c31e5771512d13430dee61abd9ff111d2202d7c0
林小如 <linxiaoruxr@163.com>
2018-04-02 09:33:06 +0800

    Add comments

--- bubble_sort/bubble_sort.js
+++ bubble_sort/bubble_sort.js
@@ -5,8 +5,8 @@ function bubbleSort(array) {
     temp;
   for (i = length - 1; 0 < i; i--) {
     for (j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        temp = array[j];
+      if (array[j] > array[j + 1]) {  // 相邻元素两两对比
+        temp = array[j];              // 元素交换
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
